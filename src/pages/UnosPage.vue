402fca48c444e60efe2a951ec855bb1f1e4d1937
etangar <etangar@veleri.hr>
2025-01-15 09:12:50 +0100

    commit

---
+++ src/pages/UnosPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <q-banner class="bg-primary text-white">
+      <template v-slot:action>
+        <q-btn label="Close" flat @click="showBanner = false" />
+      </template>
+      <div>
+        <p>{{ bannerMessage }}</p>
+      </div>
+    </q-banner>
+    
+    <q-input v-model="bannerMessage" label="Change Banner Message" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showBanner: true,
+      bannerMessage: "Poruka."
+    };
+  }
+};
+</script>
